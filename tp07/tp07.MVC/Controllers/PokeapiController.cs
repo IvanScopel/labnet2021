95a05917eb91b54b98e3307c87cc78dff0cf4e97
IvanScopel <64330182+IvanScopel@users.noreply.github.com>
2021-10-02 14:47:11 -0300

    Push de prueba

---
+++ tp07/tp07.MVC/Controllers/PokeapiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace tp07.MVC.Controllers
+{
+    public class PokeapiController : Controller
+    {
+        // GET: Pokeapi
+        public async Task<ActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://pokeapi.co/api/v2/pokemon/ditto");
+            return View("Index", new { json });
+        }
+    }
+}
